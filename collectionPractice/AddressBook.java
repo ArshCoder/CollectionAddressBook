5a0c4596d479a8b3f695149233877c4cddf39b83
CoderArsh <arshjeetb08@gmail.com>
2021-10-30 23:00:04 +0530

    Added: To add new contact to list

--- collectionPractice/AddressBook.java
+++ collectionPractice/AddressBook.java
@@ -1,4 +1,34 @@
 package com.collectionPractice;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class AddressBook {
+    List<PersonInfo> contacts = new ArrayList<PersonInfo>();
+    public void addContacts() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter FirstName: ");
+        String firstName = scanner.nextLine();
+        System.out.println("Enter LastName: ");
+        String lastName = scanner.nextLine();
+        System.out.println("Enter addres: ");
+        String address = scanner.nextLine();
+        System.out.println("Enter city: ");
+        String city = scanner.nextLine();
+        System.out.println("Enter state: ");
+        String state = scanner.nextLine();
+        System.out.println("Enter zip: ");
+        String zip = scanner.nextLine();
+        System.out.println("Enter phone number: ");
+        String phoneNumber = scanner.nextLine();
+        System.out.println("Enter email: ");
+        String email = scanner.nextLine();
+        scanner.close();
+        PersonInfo addPerson = new PersonInfo(firstName, lastName, address, city, state, zip, phoneNumber, email);
+        contacts.add(addPerson);
+        System.out.println(addPerson);
+        System.out.println(contacts);
+
+    }
 }
--- collectionPractice/TestClass.java
+++ collectionPractice/TestClass.java
@@ -1,4 +1,30 @@
 package com.collectionPractice;
 
+import java.util.Scanner;
+
 public class TestClass {
+    final static int exit = 10;
+
+    public static void main(String[] args) {
+        int option = 0;
+        AddressBook add = new AddressBook();
+        while (option != exit) {
+            System.out.println("Choice one from given below");
+            Scanner name = new Scanner(System.in);
+            System.out.println("1. Add a Person to the address book.\n 2. Edit Person Info. \n 3. Delete person Info. \n 10.Exit\n");
+            option = name.nextInt();
+
+            switch (option) {
+                case 1:
+                    add.addContacts();
+                    break;
+                case 2:
+                    break;
+                case exit:
+                    break;
+
+
+            }
+        }
+    }
 }
